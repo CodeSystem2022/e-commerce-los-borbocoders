758ead310c422e023b352442e4ef2ad33f453b47
knlc01 <kevinlopez00001@gmail.com>
2023-10-26 20:35:30 -0300

    class Product

--- backend/src/models/Product.py
+++ backend/src/models/Product.py
@@ -1,1 +1,6 @@
-
+class Product:
+    def __init__(self, product_id, product_name, description, price):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.description = description
+        self.price = price
