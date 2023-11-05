0644dcf8ac4d1ef0f62c4c551f8a21a47466002c
knlc01 <kevinlopez00001@gmail.com>
2023-11-05 20:33:30 -0300

    move decimal_encode -> utils/decimal_encode edit imports

--- backend/src/services/product_service.py
+++ backend/src/services/product_service.py
@@ -1,6 +1,6 @@
 import json
 from database.database import Connection
-from services.decimal_encoder import DecimalEncoder
+from utils.decimal_encoder import DecimalEncoder
 
 PRODUCTS_ENDPOINT = '/products'
 
