1686639dc7fd24baf3240766c00d150ff6e1b70e
Javi Gomez Allosa <106932015+javigoall@users.noreply.github.com>
2023-11-07 04:47:47 -0300

    Update products.js
    Desarrollo función products.js

---
+++ frontend/src/js/products.js
@@ -0,0 +1,71 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Taladro Percutor Bosch",
+        price: 120000,
+        quanty: 1,
+        img: "/frontend/images/taladro01.png",
+    },
+    {
+        id: 2,
+        productName: "Taladro Percutor Skill",
+        price: 78000,
+        quanty: 1,
+        img:"/frontend/images/taladro02.jpg",
+    },
+    {
+        id: 3,
+        productName: "Taladro Percutor BlackDecker",
+        price: 90000,
+        quanty: 1,
+        img: "/frontend/images/taladro03.jpg",
+    },
+
+    {
+        id: 4,
+        productName: "Taladro Percutor Gamma",
+        price: 80000,
+        quanty: 1,
+        img: "/frontend/images/taladro04.jpg",
+    },
+    {
+        id: 5,
+        productName: "Taladro Inalambrico Bosch",
+        price: 158000,
+        quanty: 1,
+        img:"/frontend/images/taladro05.jpg",
+    },
+    {
+        id: 6,
+        productName: "Taladro Inalambrico Skill",
+        price: 79000,
+        quanty: 1,
+        img: "/frontend/images/taladro06.jpg",
+    },
+
+    {
+        id: 7,
+        productName: "Taladro a bateria BlackDecker",
+        price: 103000,
+        quanty: 1,
+        img: "/frontend/images/taladro07.jpg",
+
+    },
+
+    {
+        id: 8,
+        productName: "Taladro Inalambrico Gamma",
+        price: 60000,
+        quanty: 1,
+        img: "/frontend/images/taladro08.png",
+    },
+
+    {
+        id: 9,
+        productName: "Taladro Inalambrico DeWalt",
+        price: 130000,
+        quanty: 1,
+        img: "/frontend/images/taladro09.jpg",
+    },
+ 
+]
