8caca5a707d53d96714106055c33d9f9825dc1fc
knlc01 <43591384+knlc01@users.noreply.github.com>
2023-11-10 20:10:08 -0300

    comentarios en product_service (#94)
    * update utils/decimal_ecoder - product.py
    
    * reorganización del código product_service
    
    * comentarios en product_service
    
    * comentarios en product.py

--- backend/src/models/Product.py
+++ backend/src/models/Product.py
@@ -1,3 +1,4 @@
+#Inicializa una instancia de la clase Product, con los args product_id, product_name, description, price, quantity
 class Product:
     def __init__(self, product_id, product_name, description, price, quantity):
         self.product_id = product_id
@@ -6,26 +7,32 @@ class Product:
         self.price = price
         self._quantity = quantity
 
+    #Método de clase para crear una instancia de Product con un ID específico y una cantidad.
     @classmethod
     def create_product_with_id(cls, product_id, quantity):
         return cls(product_id,"" , "", 0.0, quantity)
 
+    # Propiedad de solo lectura que devuelve el id único del producto.
     @property
     def product_id(self):
         return self._product_id
 
+    #Propiedad de solo lectura que devuelve el nombre del producto.
     @property
     def product_name(self):
         return self._product_name
 
+    #Propiedad de solo lectura que devuelve la descripción del producto.
     @property
     def description(self):
         return self._description
 
+    #Propiedad de solo lectura que devuelve el precio del producto.
     @property
     def price(self):
         return self._price
 
+    #Propiedad de solo lectura que devuelve la cantidad disponible en el inventario del producto.
     @property
     def quantity(self):
         return self._quantity
