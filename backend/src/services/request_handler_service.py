25d6bfb0b3205b515230613031d3c8913fd598f5
knlc01 <kevinlopez00001@gmail.com>
2023-11-10 21:14:11 -0300

    errores y excepciones request_handler_service.py

--- backend/src/services/request_handler_service.py
+++ backend/src/services/request_handler_service.py
@@ -4,6 +4,7 @@ from http.server import BaseHTTPRequestHandler
 from services.order_service import OrderService
 from services.product_service import ProductService
 from services.mercadopago_service import MercadopagoService
+import json
 
 load_dotenv()
 
@@ -23,21 +24,33 @@ services = {
 
 
 class RequestHandlerService(BaseHTTPRequestHandler):
-
-    
     # La clase RequestHandlerService es una clase que aplica herencia de la clase BaseHTTPRequestHandler con el objetivo de procesar las solicitudes http.
 
+    def handle_error(self, status_code, message, details=None):
+     # Método para manejar errores y enviar una respuesta de error al cliente.  Args: status_code (int): Código de estado HTTP. message (str): Mensaje de error. details (str): Detalles adicionales del error (opcional).
+        self.send_response(status_code)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+
+        response = {
+            'error': message,
+            'details': details
+        }
+
+        self.wfile.write(json.dumps(response).encode())
+    
     #  do_GET(): Se encarga de procesar las solicitudes HTTP GET derivandolas a la instancia de servicio que corresponda.
     def do_GET(self):
         path = self.path
 
         if path in services:
-            service_instance = services[path]
-            service_instance.do_GET(self)
-
+            try:
+                service_instance = services[path]
+                service_instance.do_GET(self)
+            except Exception as e:
+                self.handle_error(500, 'Error interno del servidor', str(e))
         else:
-            # handle error
-            pass
+            self.handle_error(404, 'Recurso inexistente')
 
 
     # do_POST(): Se encarga de procesar las solicitudes HTTP POST derivandolas a la instancia de servicio que corresponda.        
@@ -45,9 +58,22 @@ class RequestHandlerService(BaseHTTPRequestHandler):
         path = self.path
 
         if path in services:
-            service_instance = services[path]
-            service_instance.do_POST(self)
+            try:
+                service_instance = services[path]
+                service_instance.do_POST(self)
+            except Exception as e:
+                self.handle_error(500, 'Error interno del servidor', str(e))
         else:
-            # handle error
-            pass
+            self.handle_error(404, 'Endpoint no encontrado')
+    
+    #El método do_OPTIONS se utiliza para manejar las solicitudes OPTIONS, que son parte del protocolo CORS (Cross-Origin Resource Sharing). CORS es un mecanismo de seguridad que permite o restringe el acceso a recursos en una página web desde un dominio diferente al de la propia página.
+    # do_OPTIONS(): Se encarga de manejar las solicitudes OPTIONS.
+    def do_OPTIONS(self):
+        # Se envía una respuesta exitosa (código de estado 200).
+        self.send_response(200)        
+        # Se establecen los encabezados necesarios para permitir solicitudes CORS.
+        self.send_header('Access-Control-Allow-Origin', '*')  # Permitir a cualquier origen acceder.
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')  # Métodos permitidos.
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')  # Tipos de encabezados permitidos.
+        self.end_headers()
 
