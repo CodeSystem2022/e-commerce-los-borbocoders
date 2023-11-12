937c82d8346b3fce6b8caa5109b1f1088dd46f49
ojedam13 <80652770+ojedam13@users.noreply.github.com>
2023-11-12 19:33:14 -0300

    Se documento cart.js

--- frontend/src/js/cart.js
+++ frontend/src/js/cart.js
@@ -1,3 +1,4 @@
+// Obtiene referencias a elementos del DOM
 const deleteProductButton = document.getElementById("cart");
 const productList = document.getElementById("products");
 const cartList = document.querySelector("#cart-list tbody");
@@ -5,19 +6,24 @@ const emptyCartButton = document.getElementById("empty-cart");
 const goToCheckout = document.getElementById("checkoutLink");
 const cart = [];
 
+// Función para inicializar el carrito
 async function onInitCart(path) {
+  // Carga eventos relacionados con el carrito
   loadEvents(path);
 
+  // Si hay datos en el carrito almacenados localmente, mostrarlos
   if (getCartFromLocalStorage() != null) {
     const cartData = getCartFromLocalStorage();
     displayCart(cartData);
   }
 }
-
+// Función para cargar eventos según el contexto de la página
 function loadEvents(path) {
+  // Agrega eventos para eliminar producto y vaciar carrito
   deleteProductButton.addEventListener("click", deleteProduct);
   emptyCartButton.addEventListener("click", emptyCart);
-
+  // Agrega eventos según la página principal o de productos
+  // Agrega evento para ir a la página de checkout
   if (path === "main") {
     productList.addEventListener("click", addProduct);
     goToCheckout.addEventListener("click", function (event) {
@@ -37,11 +43,12 @@ function loadEvents(path) {
     });
   }
 }
-
+// Función para agregar un producto al carrito
 function addProduct(event) {
   event.preventDefault();
-
+  // Verifica si el elemento clickeado tiene la clase "agregar-carrito"
   if (event.target.classList.contains("agregar-carrito")) {
+    // Obtener información del producto desde el DOM
     const product = event.target.closest(".product");
     const productId = product.querySelector("a").getAttribute("product-id");
     const productName = product.querySelector("h3").textContent;
@@ -49,7 +56,7 @@ function addProduct(event) {
     const price = product.querySelector("#price").getAttribute("value");
     const category = product.querySelector("#price").getAttribute("value");
     const imageSource = product.querySelector("img").getAttribute("s");
-
+    // Crea objeto con la información del producto
     const productToAdd = {
       productId,
       productName,
@@ -61,23 +68,23 @@ function addProduct(event) {
     };
 
     console.log(productToAdd);
-
+    // Verifica si el producto ya está en el carrito
     const existingProductIndex = cart.findIndex(
       (product) => product.productId === productToAdd.productId
     );
-
+    // Actualiza la cantidad si el producto ya está en el carrito, o agregarlo
     if (existingProductIndex !== -1) {
       cart[existingProductIndex].quantity += 1;
     } else {
       cart.push(productToAdd);
     }
-
+    // Guarda el carrito en el almacenamiento local
     saveCartToLocalStorage(cart);
-
+    // Agrega el producto al carrito visualmente
     addToCart(productToAdd);
   }
 }
-
+// Función para agregar un producto al carrito visualmente
 function addToCart(product) {
   const row = document.createElement("tr");
   row.innerHTML = `
@@ -98,6 +105,7 @@ function addToCart(product) {
   cartList.appendChild(row);
 }
 
+// Función para mostrar los productos del carrito
 function displayCart(products) {
   cartList.innerHTML = "";
 
@@ -121,24 +129,27 @@ function displayCart(products) {
     cartList.appendChild(row);
   }
 }
-
+// Función para eliminar un producto del carrito
 function deleteProduct(event) {
   event.preventDefault();
-
+  // Obtiene el ID del producto a eliminar
   const productId = document
     .getElementById("delete-btn")
     .getAttribute("productId");
+  // Obtiene y actualiza el carrito desde el almacenamiento local
   const cartData = getCartFromLocalStorage();
   const updatedCart = cartData.filter(
     (product) => product.productId !== productId
   );
+  // Guarda el carrito actualizado en el almacenamiento local
   saveCartToLocalStorage(updatedCart);
-
+  // Actualiza visualmente el carrito
   addToCart(updatedCart);
   displayCart(updatedCart);
 }
-
+// Función para vaciar completamente el carrito
 function emptyCart() {
+  // Limpiar el almacenamiento local y la representación visual del carrito
   localStorage.clear();
 
   while (cartList.firstChild) {
@@ -146,29 +157,37 @@ function emptyCart() {
   }
   return false;
 }
-
+// Función para guardar el carrito en el almacenamiento local
 function saveCartToLocalStorage(cartData) {
-  localStorage.setItem('cart', JSON.stringify(cartData));
-
+  localStorage.setItem("cart", JSON.stringify(cartData));
+  // Guardar el total del carrito en el almacenamiento local
   saveTotalToLocalStorage();
 }
-
+// Función para guardar el total del carrito en el almacenamiento local
 function saveTotalToLocalStorage() {
-
-  const total = cart.reduce((total, product) => total + product.price * product.quantity, 0);
-
-  localStorage.setItem('total', JSON.stringify(total));
+  // Calcula el total sumando el precio de cada producto multiplicado por su cantidad
+  const total = cart.reduce(
+    (total, product) => total + product.price * product.quantity,
+    0
+  );
+  // Guarda el total en el almacenamiento local
+  localStorage.setItem("total", JSON.stringify(total));
 }
-
+// Función para obtener el carrito desde el almacenamiento local
 function getCartFromLocalStorage() {
-      
-  const cartData = localStorage.getItem('cart');
+  const cartData = localStorage.getItem("cart");
   return cartData ? JSON.parse(cartData) : [];
 }
-
+// Función para obtener el total del carrito desde el almacenamiento local
 function getTotalFromLocalStorage() {
-  const total = localStorage.getItem('total');
+  const total = localStorage.getItem("total");
   return total ? JSON.parse(total) : 0;
 }
-
-export { loadEvents, addProduct, onInitCart, getCartFromLocalStorage, getTotalFromLocalStorage };
+// Exporta funciones relevantes
+export {
+  loadEvents,
+  addProduct,
+  onInitCart,
+  getCartFromLocalStorage,
+  getTotalFromLocalStorage,
+};
