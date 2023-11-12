da61550d9a1b5addbfa0ba1e02c60bb10f313f01
ojedam13 <80652770+ojedam13@users.noreply.github.com>
2023-11-12 19:28:55 -0300

    Se documento main.js

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,14 +1,20 @@
+// Importa la función loadEvents desde el módulo cart.js
 import { loadEvents } from "./cart.js";
+// Importa la función onInitProducts desde el módulo products.js
 import { onInitProducts } from "./products.js";
+// Importa la función onInitCart desde el módulo cart.js
 import { onInitCart } from "./cart.js";
 
+// Función asíncrona para inicializar la aplicación
 export async function onInit() {
+  // Inicializa la sección de productos
   onInitProducts();
+  // Inicializa el carrito en la sección principal del documento HTML
   onInitCart("main");
 }
 
-    localStorage.setItem("carrito", JSON.stringify(cart));
-}
-//get item
-JSON.parse(localStorage.getItem("carrito"))
+// Guarda un objeto "cart" en el almacenamiento local (localStorage)
+localStorage.setItem("carrito", JSON.stringify(cart));
 
+// Obtene el objeto "carrito" del almacenamiento local y convertirlo de nuevo a un objeto JavaScript
+JSON.parse(localStorage.getItem("carrito"));
