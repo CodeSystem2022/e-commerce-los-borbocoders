8d3ceaf2a6312804041de5bbb622d49f43b56b90
ojedam13 <80652770+ojedam13@users.noreply.github.com>
2023-11-12 23:39:20 -0300

    Documentacion mercadopago.js

--- frontend/src/js/services/mercadopago.js
+++ frontend/src/js/services/mercadopago.js
@@ -1,34 +1,33 @@
 import { createPreference } from "./data.js";
-
+//Crea un ID de preferencia de pago utilizando la función de creación de preferencias.
 async function createPreferenceId(order) {
     
     try {
-        const preferenceId = await createPreference(order);
-
+      // Obtener el ID de preferencia utilizando la función de creación de preferencias
+      const preferenceId = await createPreference(order);
+      // Verificar si se recibió un ID de preferencia del servidor
         if (preferenceId) {
-            createMercadoPagoButton(preferenceId);
+          // Crear el botón de MercadoPago utilizando el ID de preferencia
+          createMercadoPagoButton(preferenceId);
         } else {
-            console.error('No preferenceId received from the server.');
-        }
+        console.error("No preferenceId received from the server.");
+      }
     } catch (error) {
         console.error('Error while getting preferenceId:', error);
     }
 
 }
-
+//Crea y configura el botón de pago de MercadoPago.
 async function createMercadoPagoButton(preferenceId) {
-
-
-    const mp = new MercadoPago('TEST-ea76c462-7363-4745-b32c-555a9663d47a');
-    const bricksBuilder = mp.bricks();
-
-    
-    bricksBuilder.create("wallet", "wallet_container", {
-            initialization: {
-                preferenceId: preferenceId,
-            },
-    });
-
+  // Configurar MercadoPago con la clave de prueba
+  const mp = new MercadoPago("TEST-ea76c462-7363-4745-b32c-555a9663d47a");
+  const bricksBuilder = mp.bricks();
+  // Crear el botón de pago de MercadoPago en el contenedor especificado
+  bricksBuilder.create("wallet", "wallet_container", {
+    initialization: {
+      preferenceId: preferenceId,
+    },
+  });
 }
 
 export { createPreferenceId };
