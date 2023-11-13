28cbfe9392253758fcee913bb8228b75e2443ce2
Axel Fornerod <Fornerodaxel@hotmail.com>
2023-11-13 00:47:01 -0300

    Documentacion

--- backend/src/models/cart.py
+++ backend/src/models/cart.py
@@ -1,3 +1,4 @@
+ 
 from models.product import Product
 
 
@@ -7,19 +8,30 @@ class Cart:
         self._order_id = order_id
         self._products = []
 
+    #Acceder al ID del carrito.
+
     @property
     def cart_id(self) -> int:
         return self._cart_id
 
+    #Acceder al ID de la orden.
+
     @property
     def order_id(self) -> int:
         return self._order_id
+    
+    #Agrega un producto al carrito.
 
     def add_product(self, product: Product):
         self._products.append(product)
 
+    #Obtiene la lista de productos en el carrito.
+
     def get_products(self) -> list:
         return self._products
+       
+       
+    #Calcula la cantidad total de productos en el carrito.
 
     def total_quantity(self) -> int:
         return sum(product_info["quantity"] for product_info in self._products)
--- backend/src/models/customer.py
+++ backend/src/models/customer.py
@@ -7,29 +7,42 @@ class Customer:
         self._phone = phone
         self._customer_id = customer_id
 
+    #Propiedad de solo lectura para acceder al ID del cliente
     @property
     def customer_id(self) -> int:
         return self._customer_id
 
+    #Propiedad de solo lectura para acceder al nombre del cliente.
+
     @property
     def first_name(self) -> str:
         return self._first_name
+    
+    #Propiedad de solo lectura para acceder al apellido del cliente.
 
     @property
     def last_name(self) -> str:
         return self._last_name
 
+    #Propiedad de solo lectura para acceder a la dirección de correo electrónico del cliente.
+
     @property
     def email(self) -> str:
         return self._email
+
+    #Propiedad de solo lectura para acceder a la dirección del cliente.
 
     @property
     def address(self) -> str:
         return self._address
 
+    #Propiedad de solo lectura para acceder al número de teléfono del cliente.
+
     @property
     def phone(self) -> str:
         return self._phone
+
+    #Establece el ID del cliente.
 
     def set_customer_id(self, value):
         self._customer_id = value
